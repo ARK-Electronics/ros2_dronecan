96db419d0be81fc0820231750e9677d75e028f69
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-10-24 18:04:43 -0800

    added separate publishers for flow and range

--- ros2_dronecan/ark_flow.py
+++ ros2_dronecan/ark_flow.py
@@ -9,7 +9,9 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.flow_publisher = self.create_publisher(String, 'flow', 10)
+        self.range_publisher = self.create_publisher(String, 'range', 10)
+
         self.cannode = dronecan.make_node('can0', node_id=100, bitrate=1000000)
         self.cannode_monitor = dronecan.app.node_monitor.NodeMonitor(self.cannode)
         self.dynamic_node_id_allocator = dronecan.app.dynamic_node_id.CentralizedServer(self.cannode, self.cannode_monitor)
@@ -27,20 +29,23 @@ class MinimalPublisher(Node):
 
 
     def flow_callback(self, msg):
-        # Print received DC message
         print("received flow message")
-        print(dronecan.to_yaml(msg))
-
-        # Now publish to ROS2
-        # msg = String()
-        # msg.data = 'Hello World: %d' % self.i
-        # self.publisher_.publish(msg)
+        # print(dronecan.to_yaml(msg))
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.flow_publisher.publish(msg)
         # self.get_logger().info('Publishing: "%s"' % msg.data)
         # self.i += 1
 
     def range_callback(self, msg):
-        print("received ramge message")
-        print(dronecan.to_yaml(msg))
+        print("received range message")
+        # print(dronecan.to_yaml(msg))
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.range_publisher.publish(msg)
+        # self.get_logger().info('Publishing: "%s"' % msg.data)
+        # self.i += 1
+
 
 
 def main(args=None):
