3be5a1dcffb53cbfe97bfbc82b900f3d06f0123d
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-10-25 21:36:52 -0800

    cleanup

--- ros2_dronecan/ark_flow.py
+++ ros2_dronecan/ark_flow.py
@@ -35,7 +35,6 @@ class DronecanListenerNode(Node):
     def flow_callback(self, msg):
         # PX4 CAN Node Flow Publisher
         # https://github.com/PX4/PX4-Autopilot/blob/main/src/drivers/uavcannode/Publishers/FlowMeasurement.hpp
-
         m = msg.message
         flow = SensorOpticalFlow()
 
@@ -44,72 +43,12 @@ class DronecanListenerNode(Node):
         flow.pixel_flow = [m.flow_integral[0], m.flow_integral[1]]
         flow.quality = m.quality
 
-        # print(dronecan.to_yaml(msg))
         self.flow_publisher.publish(flow)
 
-        # -------------------------------------------------------------------------------------------------
-        # Dronecan message definition
-        # -------------------------------------------------------------------------------------------------
-        # uint64 timestamp               # time since system start (microseconds)
-        # uint64 timestamp_sample
-
-        # uint32 device_id               # unique device ID for the sensor that does not change between power cycles
-
-        # float32[2] pixel_flow          # (radians) optical flow in radians where a positive value is produced by a RH rotation about the body axis
-
-        # float32[3] delta_angle         # (radians) accumulated gyro radians where a positive value is produced by a RH rotation about the body axis. Set to NaN if flow sensor does not have 3-axis gyro data.
-        # bool delta_angle_available
-
-        # float32 distance_m             # (meters) Distance to the center of the flow field
-        # bool distance_available
-
-        # uint32 integration_timespan_us # (microseconds) accumulation timespan in microseconds
-
-        # uint8 quality                  # quality, 0: bad quality, 255: maximum quality
-
-        # uint32 error_count
-
-        # float32 max_flow_rate          # (radians/s) Magnitude of maximum angular which the optical flow sensor can measure reliably
-
-        # float32 min_ground_distance    # (meters) Minimum distance from ground at which the optical flow sensor operates reliably
-        # float32 max_ground_distance    # (meters) Maximum distance from ground at which the optical flow sensor operates reliably
-
-        # uint8 MODE_UNKNOWN        = 0
-        # uint8 MODE_BRIGHT         = 1
-        # uint8 MODE_LOWLIGHT       = 2
-        # uint8 MODE_SUPER_LOWLIGHT = 3
-
-        # uint8 mode
 
     def range_callback(self, msg):
         # PX4 CAN Node Range Publisher
         # https://github.com/PX4/PX4-Autopilot/blob/main/src/drivers/uavcannode/Publishers/RangeSensorMeasurement.hpp
-
-        # -------------------------------------------------------------------------------------------------
-        # Dronecan message definition
-        # -------------------------------------------------------------------------------------------------
-        # uavcan.Timestamp timestamp
-
-        # uint8 sensor_id
-
-        # uavcan.CoarseOrientation beam_orientation_in_body_frame
-
-        # float16 field_of_view                # Radians
-
-        # uint5 SENSOR_TYPE_UNDEFINED = 0
-        # uint5 SENSOR_TYPE_SONAR     = 1
-        # uint5 SENSOR_TYPE_LIDAR     = 2
-        # uint5 SENSOR_TYPE_RADAR     = 3
-        # uint5 sensor_type
-
-        # uint3 READING_TYPE_UNDEFINED   = 0   # Range is unknown
-        # uint3 READING_TYPE_VALID_RANGE = 1   # Range field contains valid distance
-        # uint3 READING_TYPE_TOO_CLOSE   = 2   # Range field contains min range for the sensor
-        # uint3 READING_TYPE_TOO_FAR     = 3   # Range field contains max range for the sensor
-        # uint3 reading_type
-
-        # float16 range                        # Meters
-
         m = msg.message
         d = DistanceSensor()
         d.current_distance = m.range
@@ -134,7 +73,6 @@ class DronecanListenerNode(Node):
 
         d.h_fov = m.field_of_view
 
-        # print(dronecan.to_yaml(msg))
         self.range_publisher.publish(d)
 
 
